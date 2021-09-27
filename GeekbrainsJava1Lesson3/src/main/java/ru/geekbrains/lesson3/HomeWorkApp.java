12fa843290fc728db0d6f3895ad8b1eace3c6563
VorobevOleg <kukuruza-09@ya.ru>
2021-09-27 09:45:11 +0300

    Lesson3 - added methods 7,8

--- GeekbrainsJava1Lesson3/src/main/java/ru/geekbrains/lesson3/HomeWorkApp.java
+++ GeekbrainsJava1Lesson3/src/main/java/ru/geekbrains/lesson3/HomeWorkApp.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class HomeWorkApp {
     public static void  main(String[] args) {
         int[] arr1 = {0, 0, 1, 1, 1, 0, 0, 0, 1, 1};
-        int[] arr2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int[] arr2 = {1, 2, 3, 6, 4, 11, 9, 10, 7, 8};
+        int[] arr3 = {1, 2, 3, 6, 12};
 
         // Вызов методов
         System.out.println("Задание №1");
@@ -20,6 +21,10 @@ public class HomeWorkApp {
         arrInit(10,7);
         System.out.println("Задание №6");
         arrMinMax(10);
+        System.out.println("Задание №7 - " + checkBalance(arr3));
+        System.out.println("Задание №8");
+        offsetArr(arr2, -13);
+
     }
 
     //Метод №1
@@ -105,4 +110,59 @@ public class HomeWorkApp {
     }
 
     //Метод №7
+    public static boolean checkBalance(int[] arr) {
+        double sum = 0, halfSum;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        halfSum = sum / 2;
+        sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum == halfSum) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Метод №8
+    public static void offsetArr(int[] arr, int n) {
+        int offset = n % arr.length; //узнали смещение в пределах размера массива
+        int buffer;
+
+        System.out.println("Начальный массив:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // смещение вправо
+        if (offset > 0) {
+            for (int i = 0; i < Math.abs(offset); i++) {    // количество повторов смещения на 1
+                buffer = arr[arr.length -1];
+                for (int j = arr.length - 1; j > 0; j--) {  // смещение на 1 в нужную сторону
+                    arr[j] = arr[j-1];
+                }
+                arr[0] = buffer;
+            }
+        }
+
+        // смещение влево
+        if (offset < 0) {
+            for (int i = 0; i < Math.abs(offset); i++) {    // количество повторов смещения на 1
+                buffer = arr[0];
+                for (int j = 0; j < arr.length -1; j++) {   // смещение на 1 в нужную сторону
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.length -1] = buffer;
+            }
+        }
+
+        System.out.println("Массив со смещением " + n + " (" + offset + "):");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
